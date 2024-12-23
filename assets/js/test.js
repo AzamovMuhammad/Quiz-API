102f48725e2c850489e2aa50e797b0aa03c4bec0
AzamovMuhammad <azamov.dragon@gmail.com>
2024-12-23 12:13:27 +0500

    tableni chiqardim. login ni apiga mosladim. test qilib ko'rdim.

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,32 +1,42 @@
 const userId = new URLSearchParams(document.location.search).get("userId");
-const usersData = JSON.parse(localStorage.getItem("userData"));
-
-if (usersData[userId]) {
+const usersData = JSON.parse(localStorage.getItem("userData")) || [];
+console.log(usersData[userId]);
+// Validatsiya: userId va foydalanuvchi mavjudligini tekshirish
+if (!usersData[userId]) {
+  console.error("Foydalanuvchi topilmadi yoki userId noto'g'ri.");
+} else {
+  // Foydalanuvchi ma'lumotlarini olish
   const user = usersData[userId];
+
+  // DOM elementlarini yangilash
   document.querySelector(".name").innerHTML = user.username;
   document.querySelector(".avatar").src = user.userAvatar;
-} else {
-  console.log("Foydalanuvchi topilmadi");
-}
+  user.totalOfFindAnswerAll = user.totalOfFindAnswerAll || 0;
+  user.totalOfGame = user.totalOfGame || 0;
 
-let a = 0;
-let b = 0
-const userInfo = []
-function score() {
-  a = a + 1;
-  document.querySelector(".score").innerHTML = a;
-}
-function numberOfStartGame() {
-  b = b + 1;
-  document.querySelector(".numberGame").innerHTML = b;
-  window.location.href = 'login.html'
-}
+  // Foydalanuvchi statistikasi
+  function score() {
+    user.totalOfFindAnswerAll += 1;
+    document.querySelector(".score").innerHTML = user.totalOfFindAnswerAll || 0;
+    saveUserInfo();
+  }
 
-const arreyAllInfo = []
-const objectInfo = {
-  username: username,
-  avatar: avatar,
-  totalOfGame: totalOfGame,
-  totalOfFindAnswer: totalOfFindAnswer,
-  tatalofFindAnswerAll: tatalofFindAnswerAll
+  function numberOfStartGame() {
+    user.totalOfGame += 1;
+    document.querySelector(".numberGame").innerHTML = user.totalOfGame || 0;
+    saveUserInfo();
+    window.location.href = "login.html";
+  }
+
+  // Foydalanuvchi ma'lumotlarini saqlash
+  function saveUserInfo() {
+    usersData[userId] = user; // Mavjud foydalanuvchini yangilash
+    localStorage.setItem("userData", JSON.stringify(usersData));
+  }
+
+  // Dastlabki ko'rsatmalarni yuklash
+  const scoreElement = document.querySelector(".score");
+  const numberGameElement = document.querySelector(".numberGame");
+  if (scoreElement) scoreElement.innerHTML = user.totalOfFindAnswerAll;
+  if (numberGameElement) numberGameElement.innerHTML = user.totalOfGame;
 }
